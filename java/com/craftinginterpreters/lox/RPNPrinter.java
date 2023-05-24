0dc650c9883daaefb08142f081c21637c9f27f28
capu <hi@capu.tech>
2023-05-24 11:50:38 -0300

    add support for blocks and variables
    there were a lot of autogenerated files, so I reorganized the makefile
    to track them better
    
    RPNPrinter no longer has the bug where it shouted 'IDENTIFIER' whenever
    accessing a variable.

--- java/com/craftinginterpreters/lox/RPNPrinter.java
+++ java/com/craftinginterpreters/lox/RPNPrinter.java
@@ -7,19 +7,47 @@ class RPNPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
   String print(List<Stmt> statements) {
     String result = "";
     for (Stmt statement : statements) {
-      result = result + statement.accept(this);
+      result = result + statement.accept(this) + "\n";
     }
     return result;
   }
 
   @Override
+  public String visitBlockStmt(Stmt.Block stmt) {
+    String result = "{\n";
+    for (Stmt statement : stmt.statements) {
+      result = result + statement.accept(this) + "\n";
+    }
+    result = result + "}";
+    return result;
+  }
+
+  @Override
+  public String visitAssignExpr(Expr.Assign expr){
+    return expr.value.accept(this) + " " + expr.name.lexeme + " =";
+  }
+
+  @Override
+  public String visitVarStmt(Stmt.Var stmt) {
+    if(stmt.initializer != null){
+      return stmt.initializer.accept(this) + " " + stmt.name.lexeme + " var-init";
+    }
+    return stmt.name.lexeme + " var";
+  }
+
+  @Override
   public String visitExpressionStmt(Stmt.Expression stmt) {
-    return stmt.expression.accept(this) + " ;\n";
+    return stmt.expression.accept(this) + " ;";
   }
 
   @Override
   public String visitPrintStmt(Stmt.Print stmt) {
-    return stmt.expression.accept(this) + " print\n";
+    return stmt.expression.accept(this) + " print";
+  }
+
+  @Override
+  public String visitVariableExpr(Expr.Variable expr) {
+    return expr.name.lexeme;
   }
 
   @Override
@@ -37,6 +65,7 @@ class RPNPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
   @Override
   public String visitLiteralExpr(Expr.Literal expr) {
     if (expr.value == null) return "nil";
+    if (expr.value instanceof String) return "\"" + expr.value + "\"";
     return expr.value.toString();
   }
 
